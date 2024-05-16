113d967c1d4d18d0ba9e56a02d169608116f3c5f
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-16 00:17:13 -0400

    Adding section 49 Complex If Statements.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+// Prompt for input
 Console.Write("Please Enter Student's Grade: ");
 int grade = Convert.ToInt32(Console.ReadLine());
 
-if(grade > 50)
+// Simple If... Else... Statement - Decide to print pass or fail based on input
+Console.WriteLine("********************* Simple If Results *********************");
+
+Console.Write("Please Enter Student's Grade: ");
+if (grade > 50)
 {
     Console.WriteLine("Student has Passed.");
 } 
@@ -11,4 +17,41 @@ else
     Console.WriteLine("Student has Failed.");
     Console.WriteLine("Please recommend student to student affairs office.");
 }
+Console.WriteLine("********************* Simple If Results End *********************");
+
+// Complex If... Else... Statement - Decide to print pass or fail based on input
+Console.WriteLine("********************* Complex IF.. ELSE.. IF Results *********************");
+
+/* A: 86-100
+ * B: 75-84
+ * C: 65-74
+ * D: 50-64
+ * F: Less than 50
+ */
+if(grade < 0 || grade > 100)
+{
+    Console.WriteLine("Invalid Value Entered.");
+}
+else if (grade < 50)
+{
+    Console.WriteLine("Student has Failed - F");
+}
+else if (grade >= 50 && grade <= 64)
+{
+    Console.WriteLine("D");
+}
+else if (grade >= 65 && grade <= 74)
+{
+    Console.WriteLine("C");
+}
+else if (grade >= 75 && grade <= 84)
+{
+    Console.WriteLine("B");
+}
+else 
+{
+    Console.WriteLine("A - Good Job");
+}
+Console.WriteLine("********************* Complex IF.. ELSE.. IF Results End *********************");
+
 Console.WriteLine("Thank you for using this program.");
