8a56c0ea787304fb7236ae4302cc9b7a1b301892
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-15 23:08:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Please Enter Student's Grade: ");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+if(grade > 50)
+{
+    Console.WriteLine("Student has Passed.");
+} 
+else
+{
+    Console.WriteLine("Student has Failed.");
+    Console.WriteLine("Please recommend student to student affairs office.");
+}
+Console.WriteLine("Thank you for using this program.");
